0c29df1b05b98642d7b82f7fa8944778ac007813
frascu <francescoscuccimarri@gmail.com>
2020-10-14 16:39:07 +0200

    add lyfe cycle demo

---
+++ src/main/java/it/scuccimarri/springdi/services/CustomBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package it.scuccimarri.springdi.services;
+
+import org.springframework.beans.*;
+import org.springframework.beans.factory.config.*;
+import org.springframework.stereotype.*;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+
+        return bean;
+    }
+}
